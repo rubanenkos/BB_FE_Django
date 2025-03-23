98cb2a34a3c19b17387c0e32faa8318e001c7a4a
rubanenkos <sergii.rubanenko@gmail.com>
2025-03-23 17:52:40 +0200

    Added styles

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.urls import  path
 from .views import index, about
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', index, name='home'),
     path('about/', about, name='about'),
 ]
